3d4b82e49989c357b86dd9da21304fb2fd43ae75
WongYC-66 <yengchien@hotmail.com>
2024-11-11 19:08:04 +0800

    add: pocketto and setup recipe database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,52 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import path = require("path");
+
+import { faker } from '@faker-js/faker';
+import { RecipeItem, Recipe } from "./models/model"
+import { DatabaseManager } from 'pocketto';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('a');
+// pocketto setup
+DatabaseManager.connect('default', {
+  encryption: false,
+  silentConnect: false,
+  dbName: 'my-database',
+  adapter: 'leveldb',
+  auth: {
+      username: 'admin',
+      password: 'password'
+  },
+});
+
+
+// set the view engine to ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Home 
+app.get('/', async(req: Request, res: Response) => {
+  const allRecipes = await Recipe.all()
+  console.log(allRecipes)
+  res.render("home", {allRecipes});
+});
+
+// Create New Recipe
+app.get('/newRecipe', async (req: Request, res: Response) => {
+  
+  const newRecipe = new Recipe({
+    name: faker.food.dish(),                          // name 
+    description: faker.food.description(),            // description
+    popularity: faker.number.int({ min: 1, max: 5 }), // popularity
+  });
+
+  console.log(newRecipe)
+  await newRecipe.save()
+  res.render('home');
 });
 
 app.listen(port, () => {
---
+++ src/models/model.ts
@@ -0,0 +1,20 @@
+import { Model } from 'pocketto';
+
+export class RecipeItem extends Model {
+    static dbName = 'my-database';
+    static collectionName = 'RecipeItems';
+
+    name!: string;
+    quantity!: number;
+    unit!: string;
+
+}
+
+export class Recipe extends Model {
+    static dbName = 'my-database';
+    static collectionName = 'Recipes';
+
+    name!: string;
+    description!: string;
+    popularity!: number;
+}
